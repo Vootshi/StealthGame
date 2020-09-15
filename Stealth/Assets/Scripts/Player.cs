89f03acb5b7406979e3ecd0b3c71540520f53949
Vootshi <52784078+Vootshi@users.noreply.github.com>
2020-09-16 03:28:52 +0500

    Adding a project

---
+++ Stealth/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static event System.Action LevelEnd;
+
+    public float moveSpeed = 7f;
+    public float smoothMoveTime = .1f;
+    public float turnSpeed = 8f;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+    bool disabled;
+    
+    Rigidbody rigidbody;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if (!disabled)
+        {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, turnSpeed * Time.deltaTime * inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+
+    void Disable()
+    {
+        disabled = true;
+    }
+
+    private void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            Disable();
+            LevelEnd();
+        }
+    }
+}
